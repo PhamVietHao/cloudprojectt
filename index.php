04f3f4c8ea4ef0e57850aedc0059282431984701
PhamVietHao <89537529+PhamVietHao@users.noreply.github.com>
2022-09-17 16:44:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	
+	<title>Happy Land site</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body class="bodyStyle">
+
+	<div id="header" class="mainHeader">
+		<hr>
+		<div class="center"><H1>Happy land's Example Social Research Page</H1></div>
+	</div>
+	<br>
+	<hr>
+	<div class="topnav">
+		<a href="#aboutUs">About Us</a>
+		<a href="#contactUs">Contact Us</a>
+		<a href="query.php">Query</a>
+	</div>
+	<hr>
+	<div id="mainContent">
+
+		<div id="mainPictures" class="center">
+
+			<hr>
+			<p>Welcome to our page, here we have everything you need  </p>
+			<br>
+			<table>
+				<tr>
+				    <td>
+						<div class="cursiveText">We provide data for a variety of areas including basic demographics and development statistics.</div>
+
+					</td>
+				</tr>
+			</table>
+			<hr>
+		</div>
+	</div>
+
+	<div id="aboutUs" class="center">
+		<hr>
+		<div>
+			<h2>About Us</h2>
+		</div>
+			<table>
+				<tr>
+					<td><figure><img src="image.jpg" height=auto width="400"><figcaption>Shirley Rodriguez</figcaption></figure></td>
+				</tr>
+					<tr><td><p>Hope you guys can find this page helpfull <br>Because everything here is <span class="bolded">FREE</span>  </p></td>
+				</tr>
+			</table>
+			<hr>
+		</div>
+
+	<div id="contactUs" align="center">
+		<hr>
+		<div>
+			<h2>Contact Us</h2>
+		</div>
+		<table>
+			<tr>
+				<td><img src="Logo.png" height=auto width="120"></td>
+			</tr>
+		</table>
+		<div><p>Somewhere in Viet Nam</p></div>
+		<div>
+			<h3>Hours</h3>
+		</div>
+		<div>Weekdays: 6:00am - 6:00pm<br>Saturday: 7:00am - 7:00pm<br>Closed on Sundays</div>
+	</div>
+
+</body>
+</html>
